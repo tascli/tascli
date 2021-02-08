860e0f5d4854d8228877979f46b8da95220d041f
kramerbastian <kramerbastian@gmx.net>
2021-02-08 12:21:59 +0100

    add anoption to name the task by passing an argument

--- lib/commands/create.dart
+++ lib/commands/create.dart
@@ -1,11 +1,15 @@
 import 'package:prompts/prompts.dart' as prompts;
-import 'package:tascli/helpers/types.dart';
 import 'package:tascli/helpers/json.dart';
 
-void createLocalTask(Map<String, dynamic> json) {
+void createLocalTask(Map<String, dynamic> json, String taskName) {
   Map<String, dynamic> task = new Map();
 
-  task['name'] = prompts.get('Enter the name:');
+  if (taskName == null) {
+    task['name'] = prompts.get('Enter the name:');
+  } else {
+    task['name'] = taskName;
+  }
+
   task['description'] = prompts.get('And the description?');
   task['date'] = prompts.get('When should this task finished?');
   task['favourite'] = prompts.getBool('Do you love it?');
