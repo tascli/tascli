49c045c2389354ea3b1b0584c2dbc9d25ddc6691
kramerbastian <kramerbastian@gmx.net>
2021-02-08 13:18:02 +0100

    Revert "implements all helpers and create pubspec.yaml"
    This reverts commit 928fc0d0e91c500683768a50fffb093eaeb39a59.

--- lib/helpers/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:ansicolor/ansicolor.dart';
-
-AnsiPen green = new AnsiPen()..green();
-AnsiPen red = new AnsiPen()..red();
-AnsiPen cyan = new AnsiPen()..cyan();
-AnsiPen gray = new AnsiPen()..gray();
-AnsiPen yellow = new AnsiPen()..yellow();
