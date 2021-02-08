9e5a1dfe09f668b8107cf27e3e1a4bb6fecc0f94
kramerbastian <kramerbastian@gmx.net>
2021-02-08 13:17:46 +0100

    Revert "add anoption to name the task by passing an argument"
    This reverts commit 860e0f5d4854d8228877979f46b8da95220d041f.

--- lib/commands/create.dart
+++ lib/commands/create.dart
@@ -1,15 +1,11 @@
 import 'package:prompts/prompts.dart' as prompts;
+import 'package:tascli/helpers/types.dart';
 import 'package:tascli/helpers/json.dart';
 
-void createLocalTask(Map<String, dynamic> json, String taskName) {
+void createLocalTask(Map<String, dynamic> json) {
   Map<String, dynamic> task = new Map();
 
-  if (taskName == null) {
-    task['name'] = prompts.get('Enter the name:');
-  } else {
-    task['name'] = taskName;
-  }
-
+  task['name'] = prompts.get('Enter the name:');
   task['description'] = prompts.get('And the description?');
   task['date'] = prompts.get('When should this task finished?');
   task['favourite'] = prompts.getBool('Do you love it?');
